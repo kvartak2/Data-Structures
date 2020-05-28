baecb8da0b13f3ba70620868214e94d9ddfa9ef7
Kasturi Vartak <54905094+kvartak2@users.noreply.github.com>
2020-05-28 11:15:51 -0400

    Add files via upload

---
+++ doublly_linked_list.py
@@ -0,0 +1,89 @@
+class Node: 
+    def __init__(self, data): 
+        self.data = data  
+        self.next = self  
+  
+class LinkedList:   
+    def __init__(self): 
+        self.head = None
+    
+    def printlist(self):
+        temp=list.head
+        while(temp.next!=list.head):
+            print(temp.data)
+            temp=temp.next
+        print(temp.data)
+        
+    def append(self,val):
+        temp=Node(val)
+        p=list.head
+        while(p.next!=list.head):
+            p=p.next
+        p.next=temp
+        temp.next=list.head
+        
+        
+    def removeNthFromEnd(self, n):
+        temp=list.head
+        if(temp.next==list.head):
+            print("only head exist")
+        else:
+            p=q=list.head
+            pos=1
+            while(q.next!=list.head):
+                q=q.next
+                pos=pos+1
+            #print(pos)
+            tobedelete=pos-n
+            list.delete(tobedelete)
+            
+    
+    def delete(self,pos):
+        if(pos==0 or list.head.next==list.head):
+            print("can't delete head node")
+        else:
+            p=list.head
+            q=list.head.next
+            npos=0
+            while(npos!=pos-1):
+                p=p.next
+                if(q.next==None):
+                    print("exceeded noeds")
+                    break
+                q=q.next
+                npos=npos+1
+            p.next=q.next
+            q=q.next
+        
+list=LinkedList()
+list.head=Node(10)
+list.printlist()
+print("---------")
+
+list.append(20)
+list.printlist()
+print("---------")
+
+list.append(30)
+list.printlist()
+print("---------")
+
+list.append(40)
+list.printlist()
+print("---------")
+
+list.append(50)
+list.printlist()
+print("---------")
+
+list.append(60)
+list.printlist()
+print("---------")
+
+list.delete(3)
+list.printlist()
+print("---------")
+
+list.removeNthFromEnd(4)
+list.printlist()
+print("---------")
---
+++ singly_linked_list.py
@@ -0,0 +1,74 @@
+class Node: 
+    def __init__(self, data): 
+        self.data = data  
+        self.next = None
+  
+class LinkedList:   
+    def __init__(self): 
+        self.head = None
+        
+    def printlist(self):
+        temp=self.head
+        while(temp):
+            print(temp.data)
+            temp=temp.next
+
+    def append(self,val):
+        temp=Node(val)
+        p=list.head
+        while(p.next):
+            p=p.next
+        p.next=temp
+        
+    def insert(self,val,pos):
+        if(list.head.next==None):
+            list.append(val)
+        else:
+            temp=Node(val)
+            npos=0
+            p=list.head
+            q=list.head.next
+            while(npos==pos-1):
+                p=p.next
+                q=q.next
+                npos=npos+1
+            p.next=temp
+            temp.next=q
+
+    def delete(self,pos):
+        if(pos==0 or list.head.next==None):
+            print("can't delete head node")
+        else:
+            p=list.head
+            q=list.head.next
+            npos=0
+            while(npos!=pos-1):
+                p=p.next
+                if(q.next==None):
+                    print("exceeded noeds")
+                    break
+                q=q.next
+                npos=npos+1
+            p.next=q.next
+            q=q.next
+       
+list=LinkedList()
+list.head=Node(10)
+
+list.printlist()
+print("---------")
+
+list.insert(30,1)
+list.printlist()
+print("---------")
+
+list.insert(40,2)
+list.printlist()
+print("---------")
+
+list.append(14)
+list.printlist()
+print("---------")
+
+list.delete(5)
+list.printlist()
