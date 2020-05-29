bedad9e986c149199ee5b21be502d340bab0b47c
Kasturi Vartak <54905094+kvartak2@users.noreply.github.com>
2020-05-29 01:32:55 -0400

    Update and rename doubly_circular_linked_list to doubly_circular_linked_list.py

--- doubly_circular_linked_list.py
+++ doubly_circular_linked_list.py
@@ -55,7 +55,7 @@ class LinkedList:
             npos=0
             while(npos!=pos-1):
                 p=p.next
-                if(q.next==None):
+                if(q.next==list.head):
                     print("exceeded noeds")
                     return
                 q=q.next
